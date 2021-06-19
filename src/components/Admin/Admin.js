1e11f6a1020a45b773b37bcd439ba685d1c2d9f9
iswoad <iswoadansary@gmail.com>
2021-06-19 18:36:07 +0600

    successful form data sent to backend

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,11 +1,33 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 import { useForm } from "react-hook-form";
 import "./Admin.css";
 
 const Admin = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const [ imageUrl, setImageUrl ] = useState(null);
+
+    const onSubmit = data => {
+        const bookData = {
+            name:data.bookName,
+            aName: data.authorName,
+            price: data.bookPrice,
+            imageLink: imageUrl
+        }
+        const url = `http://localhost:4000/addBook`
+        console.log(bookData);
+        fetch(url,{
+            method: 'POST',
+            headers:{
+                'content-Type' : 'application/json'
+            },
+            body: JSON.stringify(bookData)
+        })
+        .then(res => console.log('server side response', res))
+    };
+
     const handleImageUpload = (event)=> {
         const imageData = new FormData();
         imageData.set('key',  'e337ee47dfd720c893875c61d5c17f72');
@@ -13,24 +35,26 @@ const Admin = () => {
 
         axios.post('https://api.imgbb.com/1/upload', imageData)
           .then(function (response) {
-            console.log(response.data.data.display_url);
+            setImageUrl(response.data.data.display_url);
           })
           .catch(function (error) {
             console.log(error);
           });
         console.log(event.target.file);
     }
+
+
     return (
         <div>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <h1>Book Name</h1>
-                <input type= "text" defaultValue="" {...register("example")} />
+                <input type= "text" name="bookName" defaultValue="" {...register("bookName")}/>
                 <br></br>
                 <h1>Author Name</h1>
-                <input type= "text" defaultValue="" {...register("example")} />
+                <input type= "text" name="authorName" defaultValue="" {...register("authorName")}/>
                 <br></br>
                 <h1>Book Price</h1>
-                <input type= "text" defaultValue="" {...register("example")} />
+                <input type= "text" name="bookPrice" defaultValue="" {...register("bookPrice")} />
                 <br></br>
                 <h1>Book Image</h1>
                 <input type = "file" onChange = {handleImageUpload}/>
